d601d80ce99168bedb0feb45582c89a4f4240c44
mjrouser <matthew@mjrouser.com>
2017-08-08 21:20:30 -0400

    finished

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -42,74 +42,23 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports, __webpack_require__) {
-
-	'use strict';
-	
-	var _mortgage = __webpack_require__(1);
-	
-	var mortgage = _interopRequireWildcard(_mortgage);
-	
-	function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
-	
-	document.getElementById('calcBtn').addEventListener('click', function () {
-	    var principal = document.getElementById("principal").value;
-	    var years = document.getElementById("years").value;
-	    var rate = document.getElementById("rate").value;
-	
-	    var _mortgage$calculateAm = mortgage.calculateAmortization(principal, years, rate);
-	
-	    var monthlyPayment = _mortgage$calculateAm.monthlyPayment;
-	    var monthlyRate = _mortgage$calculateAm.monthlyRate;
-	    var amortization = _mortgage$calculateAm.amortization;
-	
-	    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
-	    document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
-	    amortization.forEach(function (month) {
-	        return console.log(month);
-	    });
-	});
-
-/***/ }),
-/* 1 */
 /***/ (function(module, exports) {
 
 	"use strict";
 	
-	Object.defineProperty(exports, "__esModule", {
-	    value: true
+	var url = "rates.json";
+	
+	fetch(url).then(function (response) {
+	    return response.json();
+	}).then(function (rates) {
+	    var html = '';
+	    rates.forEach(function (rate) {
+	        return html += "<tr><td>" + rate.name + "</td><td>" + rate.years + "</td><td>" + rate.rate + "%</td></tr>";
+	    });
+	    document.getElementById("rates").innerHTML = html;
+	}).catch(function (e) {
+	    return console.log(e);
 	});
-	var calculateMonthlyPayment = exports.calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
-	    var monthlyRate = 0;
-	    if (rate) {
-	        monthlyRate = rate / 100 / 12;
-	    }
-	    var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
-	    return { principal: principal, years: years, rate: rate, monthlyPayment: monthlyPayment, monthlyRate: monthlyRate };
-	};
-	
-	var calculateAmortization = exports.calculateAmortization = function calculateAmortization(principal, years, rate) {
-	    var _calculateMonthlyPaym = calculateMonthlyPayment(principal, years, rate);
-	
-	    var monthlyRate = _calculateMonthlyPaym.monthlyRate;
-	    var monthlyPayment = _calculateMonthlyPaym.monthlyPayment;
-	
-	    var balance = principal;
-	    var amortization = [];
-	    for (var y = 0; y < years; y++) {
-	        var interestY = 0; //Interest payment for year y
-	        var principalY = 0; //Principal payment for year y
-	        for (var m = 0; m < 12; m++) {
-	            var interestM = balance * monthlyRate; //Interest payment for month m
-	            var principalM = monthlyPayment - interestM; //Principal payment for month m
-	            interestY = interestY + interestM;
-	            principalY = principalY + principalM;
-	            balance = balance - principalM;
-	        }
-	        amortization.push({ principalY: principalY, interestY: interestY, balance: balance });
-	    }
-	    return { monthlyPayment: monthlyPayment, monthlyRate: monthlyRate, amortization: amortization };
-	};
 
 /***/ })
 /******/ ]);
